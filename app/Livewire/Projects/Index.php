0f3ec2d3d74345188683cfaddb6d22e70f5ad7d9
Welen1911 <welenalmeida2210@gmail.com>
2024-10-09 08:30:14 -0300

    Componentes livewire

---
+++ app/Livewire/Projects/Index.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Livewire\Projects;
+
+use App\Models\Project;
+use Livewire\Component;
+
+class Index extends Component
+{
+    #[Computed()]
+    public function projects () {
+        return Project::query()->inRandomOrder()->get();
+    }
+
+    public function render()
+    {
+        return view('livewire.projects.index');
+    }
+}
---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.app>
+    <livewire:projects.show :$project/>
+    <livewire:projects.proposal :$project/>
+
+</x-layouts.app>
